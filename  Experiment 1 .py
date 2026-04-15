5c95cc27d0e19646389f8ebfd5e1edae25862056
axay-oss <axaykho@gmail.com>
2026-04-15 21:43:25 +0530

    Add files via upload

---
+++  Experiment 1 .py
@@ -0,0 +1,31 @@
+# Integer
+a = 10
+print("Integer:", a, "Type:", type(a))
+
+# Float
+b = 10.5
+print("Float:", b, "Type:", type(b))
+
+# String
+c = "Hello Python"
+print("String:", c, "Type:", type(c))
+
+# Boolean
+d = True
+print("Boolean:", d, "Type:", type(d))
+
+# List
+e = [1, 2, 3]
+print("List:", e, "Type:", type(e))
+
+# Tuple
+f = (10, 20, 30)
+print("Tuple:", f, "Type:", type(f))
+
+# Dictionary
+g = {"name": "Axay", "age": 20}
+print("Dictionary:", g, "Type:", type(g))
+
+# Set
+h = {1, 2, 3}
+print("Set:", h, "Type:", type(h))
