2d73b9a1ba894cf5b50f9f2396e7a1949112b0e3
axay-oss <axaykho@gmail.com>
2026-04-15 21:44:05 +0530

    Add files via upload

---
+++  Experiment 2.py
@@ -0,0 +1,32 @@
+# Addition two numbers
+a = 10
+b = 20
+sum = a + b
+print("Sum is:", sum)
+
+# Variables and their Types
+a = 10
+b = 10.5
+c = "Hello"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# Type Casting
+a = 10
+b = float(a)   # int to float
+c = str(a)     # int to string
+print(b, type(b))
+print(c, type(c))
+
+# Logical Operators
+a = True
+b = False
+
+print("AND:", a and b)
+print("OR:", a or b)
+print("NOT:", not a)
+Ajun kahi images astil tar pathva 👍
